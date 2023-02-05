0d4e1ca695dc3aaf77aeab35e1eacf2f9595057a
Odirlei Borgert <=>
2023-02-05 18:55:18 -0300

    feat(Categorias): Criado resource para categorias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriasModule } from './categorias/categorias.module';
 
 @Module({
-  imports: [],
+  imports: [CategoriasModule],
   controllers: [AppController],
   providers: [AppService],
 })
