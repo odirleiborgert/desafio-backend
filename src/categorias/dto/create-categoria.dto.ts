ff3354b3c7d0ba531236156e07e3d25684bbdac8
Odirlei Borgert <=>
2023-02-06 14:45:45 -0300

    feat: Implementado validação para categorias

--- src/categorias/dto/create-categoria.dto.ts
+++ src/categorias/dto/create-categoria.dto.ts
@@ -1,2 +1,9 @@
+import { IsNotEmpty, Length } from 'class-validator';
 
-export class CreateCategoriaDto {}
+export class CreateCategoriaDto {
+
+    @IsNotEmpty({ message: 'Nome não pode ser vazio' })
+    @Length(2, 20, { message: 'Nome precisa ter entre 3 e 10 caracteres' })
+    nome: string;
+
+}
--- src/categorias/dto/update-categoria.dto.ts
+++ src/categorias/dto/update-categoria.dto.ts
@@ -1,4 +1,10 @@
+import { IsNotEmpty, Length } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCategoriaDto } from './create-categoria.dto';
 
-export class UpdateCategoriaDto extends PartialType(CreateCategoriaDto) {}
+export class UpdateCategoriaDto extends PartialType(CreateCategoriaDto) {
+
+    @IsNotEmpty({ message: 'Nome não pode ser vazio' })
+    @Length(2, 20, { message: 'Nome precisa ter entre 3 e 10 caracteres' })
+    nome: string;
+}
