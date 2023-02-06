be3b19d40b4ecfe256d0a4eaa7e9255166d5e2fd
Odirlei Borgert <=>
2023-02-06 13:23:58 -0300

    feat: Criação do CRUD para veículos

--- src/veiculos/veiculos.service.ts
+++ src/veiculos/veiculos.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm/dist';
+import { Repository } from 'typeorm';
 import { CreateVeiculoDto } from './dto/create-veiculo.dto';
 import { UpdateVeiculoDto } from './dto/update-veiculo.dto';
+import { Veiculo } from './entities/veiculo.entity';
 
 @Injectable()
 export class VeiculosService {
+
+  constructor(
+    @InjectRepository(Veiculo)
+    private veiculossRepository: Repository<Veiculo>,
+  ) {}
+
   create(createVeiculoDto: CreateVeiculoDto) {
-    return 'This action adds a new veiculo';
+    return this.veiculossRepository.save(createVeiculoDto);
   }
 
   findAll() {
-    return `This action returns all veiculos`;
+    return this.veiculossRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} veiculo`;
+    return this.veiculossRepository.findOneBy({ id });
   }
 
   update(id: number, updateVeiculoDto: UpdateVeiculoDto) {
-    return `This action updates a #${id} veiculo`;
+    return this.veiculossRepository.update(id, updateVeiculoDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} veiculo`;
+    return this.veiculossRepository.delete(id);
   }
 }
