702aa6425e3b0e98dfcd7c5e39d42ef3d71360b6
Odirlei Borgert <=>
2023-02-07 10:15:38 -0300

    feat: Liberando Cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
   await app.listen(3000);
 }
 bootstrap();
